c50aea8030da3fe4baec91a23a775b9fc7d0c384
Luc Bourlier <luc.bourlier@typesafe.com>
2014-03-19 13:36:09 +0100

    Step 0

--- app/model/User.scala
+++ app/model/User.scala
@@ -5,7 +5,6 @@ import play.api.libs.iteratee.Concurrent
 import akka.actor.ActorRef
 import akka.actor.Props
 import akka.actor.ActorSelection
-import views.html.message
 import akka.actor.PoisonPill
 import play.api.libs.json.JsValue
 import play.api.libs.json.Json
@@ -13,25 +12,7 @@ import play.api.libs.json.Json
 class User(channel: Concurrent.Channel[JsValue], board: ActorSelection) extends Actor {
 
   def receive = {
-    case Action("go", _, _, _) =>
-      board ! Board.SubscribeAndGetAll
-    case Action("msg", Some(user), Some(text), _) =>
-      board ! Board.AddMessage(Message(user, text))
-    case Action("edit", Some(name), _, Some(true)) =>
-      board ! Board.StartEdit(name)
-    case Action("edit", Some(name), _, Some(false)) =>
-      board ! Board.StopEdit(name)
-    case User.ClientConnectionLost =>
-      board ! Board.Unsubscribe
-      self ! PoisonPill
-    case Board.Messages(messages) =>
-      messages.foreach { m =>
-        import model.ActionJson._
-        channel.push(Json.toJson(Action("msg", Some(m.name), Some(m.text), None)))
-      }
-    case Board.Editors(editors) =>
-      import model.ActionJson._
-      channel.push(Json.toJson(Action("editors", None, Some(editors.mkString(", ")), None)))
+    case () =>
   }
 
   override def postStop() {
--- public/javascripts/websocket.js
+++ public/javascripts/websocket.js
@@ -4,78 +4,30 @@ function action(msgString) {
 
 	var msg = JSON.parse(msgString);
 	
-	if (msg.id == "msg") {
-		actionMsg(msg);
-	} else if (msg.id == "editors") {
-		actionEditors(msg);
-	}
-}
-
-function actionMsg(msg) {
-
-    var divMessage = document.createElement("div");
-    divMessage.className = "message";
-    	
-    var spanName = document.createElement("span");
-    spanName.className = "msg-name";
-    spanName.appendChild(document.createTextNode(msg.name));
-    divMessage.appendChild(spanName);
-
-    var spanSeparator = document.createElement("span");
-    spanSeparator.className = "msg-separator";
-    spanSeparator.appendChild(document.createTextNode(" - "));
-    divMessage.appendChild(spanSeparator)
-    
-    var divText = document.createElement("div");
-    divText.className = "msg-text";
-    divText.appendChild(document.createTextNode(msg.data));
-    divMessage.appendChild(divText)
-
-	var board = document.getElementById("board-container");
-	board.appendChild(divMessage);
-}
-
-function actionEditors(msg) {
+	// ---
 	
-	var board = document.getElementById("board-editors");
-	board.innerHTML = msg.data;
 }
 
 function sendMessage() {
 	var inputName = document.getElementById("input-name").value;
 	var inputText = document.getElementById("input-text").value;
-	
-	var action = {
-	  "id" : "msg",
-	  "name" : inputName,
-	  "data" : inputText
-	};
 
-	socket.send(JSON.stringify(action));
+	// ---
+
 }
 
 function textFocus() {
 	var inputName = document.getElementById("input-name").value;
-	
-	var action = {
-			  "id" : "edit",
-			  "name" : inputName,
-			  "focus" : true
-			};
 
-			socket.send(JSON.stringify(action));
+	// ---
+
 }
 
 function textBlur() {
 	var inputName = document.getElementById("input-name").value;
-	
-	var action = {
-			  "id" : "edit",
-			  "name" : inputName,
-			  "focus" : false
-			};
 
-	socket.send(JSON.stringify(action));
+	// ---
+
 }
 
 function createWebSocket() {
@@ -84,17 +36,18 @@ function createWebSocket() {
 
 	socket.onopen = function() {
 		console.log("websocket open");
-		var board = document.getElementById("board-container");
-		var board = document.getElementById("board-editors");
-		board.innerHTML = "";
-		var action = {
-			"id" : "go"
-		};
-		socket.send(JSON.stringify(action));
+		var bcontainer = document.getElementById("board-container");
+		bcontainer.innerHTML = "";
+		var beditors = document.getElementById("board-editors");
+		beditors.innerHTML = ""
+	
+	    // ---
+
 	}
 
 	socket.onmessage = function(event) {
-		action(event.data);
+		// ---
+
 	}
 
 	socket.onclose = function() {
@@ -110,3 +63,36 @@ function createWebSocket() {
 }
 
 createWebSocket();
+
+// --- html modification methods ---
+
+function addMessage(name, text) {
+
+    var divMessage = document.createElement("div");
+    divMessage.className = "message";
+    	
+    var spanName = document.createElement("span");
+    spanName.className = "msg-name";
+    spanName.appendChild(document.createTextNode(name));
+    divMessage.appendChild(spanName);
+
+    var spanSeparator = document.createElement("span");
+    spanSeparator.className = "msg-separator";
+    spanSeparator.appendChild(document.createTextNode(" - "));
+    divMessage.appendChild(spanSeparator)
+    
+    var divText = document.createElement("div");
+    divText.className = "msg-text";
+    divText.appendChild(document.createTextNode(text));
+    divMessage.appendChild(divText)
+
+	var board = document.getElementById("board-container");
+	board.appendChild(divMessage);
+}
+
+function setEditors(editorString) {
+	
+	var board = document.getElementById("board-editors");
+	board.innerHTML = editorString;
+}
+
